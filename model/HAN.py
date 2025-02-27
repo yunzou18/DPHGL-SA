b8ab5666bbb99b600ce6089926d52fca2b131093
yunzou18 <114248517+yunzou18@users.noreply.github.com>
2025-02-27 20:54:35 +0800

    Update HAN.py

--- model/HAN.py
+++ model/HAN.py
@@ -4,11 +4,10 @@ from typing import Dict, List, Union
 from torch.nn import Embedding
 import torch
 class HAN(nn.Module):
-    def __init__(self, in_channels:Union[int, Dict[str, int]],out_channels, hidden_channels,graph,orther_count):  #这里添加了一个数据
+    def __init__(self, in_channels:Union[int, Dict[str, int]],out_channels, hidden_channels,graph,orther_count):  
         super(HAN, self).__init__()
         # H, D = self.heads, self.out_channels // self.heads
         self.conv1 = HANConv(in_channels, hidden_channels, graph.metadata(), heads=4)
-        #64原本为hidden_channels
         self.embedding = Embedding(orther_count + 1, hidden_channels, sparse=False)
 
     def forward(self, data):
@@ -25,4 +24,4 @@ class HAN(nn.Module):
         output =  a
 
 
-        return output ,self.embedding.weight #这里要不要返回各种节点个数考虑
+        return output ,self.embedding.weight 
